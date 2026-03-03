90d193164a876b5b1b56b900d99d76a147c91a6b
KEL0002 <kel0002@users.noreply.github.com>
2026-03-03 20:27:26 +0100

    class, no control

--- main.py
+++ main.py
@@ -1,305 +1,315 @@
 import copy
 import random
 
-board = [[0]*10 for _ in range(20)]
 
-def boom():
-    print("""
+class Tetris:
+    def __init__(self):
+        self.board = [[0]*10 for _ in range(20)]#
+        self.pieces_0 = {
+            "I": [[0, 0, 0, 0],
+                  [1, 1, 1, 1],
+                  [0, 0, 0, 0],
+                  [0, 0, 0, 0]],
+            "J": [[1, 0, 0],
+                  [1, 1, 1],
+                  [0, 0, 0]],
+            "L": [[0, 0, 1],
+                  [1, 1, 1],
+                  [0, 0, 0]],
+            "O": [[1, 1],
+                  [1, 1]],
+            "S": [[0, 1, 1],
+                  [1, 1, 0],
+                  [0, 0, 0]],
+            "T": [[0, 1, 0],
+                  [1, 1, 1],
+                  [0, 0, 0]],
+            "Z": [[1, 1, 0],
+                  [0, 1, 1],
+                  [0, 0, 0]]
+        }
+        self.pieces_90 = {
+            "I": [[0, 0, 1, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 1, 0]],
+            "J": [[0, 1, 1],
+                  [0, 1, 0],
+                  [0, 1, 0]],
+            "L": [[0, 1, 0],
+                  [0, 1, 0],
+                  [0, 1, 1]],
+            "O": [[1, 1],
+                  [1, 1]],
+            "S": [[0, 1, 0],
+                  [0, 1, 1],
+                  [0, 0, 1]],
+            "T": [[0, 1, 0],
+                  [0, 1, 1],
+                  [0, 1, 0]],
+            "Z": [[0, 0, 1],
+                  [0, 1, 1],
+                  [0, 1, 0]]
+        }
+        self.pieces_180 = {
+            "I": [[0, 0, 0, 0],
+                  [0, 0, 0, 0],
+                  [1, 1, 1, 1],
+                  [0, 0, 0, 0]],
+            "J": [[0, 0, 0],
+                  [1, 1, 1],
+                  [0, 0, 1]],
+            "L": [[0, 0, 0],
+                  [1, 1, 1],
+                  [1, 0, 0]],
+            "O": [[1, 1],
+                  [1, 1]],
+            "S": [[0, 0, 0],
+                  [0, 1, 1],
+                  [1, 1, 0]],
+            "T": [[0, 0, 0],
+                  [1, 1, 1],
+                  [0, 1, 0]],
+            "Z": [[0, 0, 0],
+                  [1, 1, 0],
+                  [0, 1, 1]]
+        }
+        self.pieces_270 = {
+            "I": [[0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 1, 0, 0]],
+            "J": [[0, 1, 0],
+                  [0, 1, 0],
+                  [1, 1, 0]],
+            "L": [[1, 1, 0],
+                  [0, 1, 0],
+                  [0, 1, 0]],
+            "O": [[1, 1],
+                  [1, 1]],
+            "S": [[1, 0, 0],
+                  [1, 1, 0],
+                  [0, 1, 0]],
+            "T": [[0, 1, 0],
+                  [1, 1, 0],
+                  [0, 1, 0]],
+            "Z": [[0, 1, 0],
+                  [1, 1, 0],
+                  [1, 0, 0]]
+        }
+
+        self.piece = self.randomNewPiece(None)
+        self.piece_next = self.randomPieceType()
+        self.piece_held = self.randomPieceType()
+        self.canHold = True
+
+    @staticmethod
+    def boom():
+        print("""
+                                     .               
                                  .               
-                             .               
-                             .       :       
-                             :      .        
-                    :..   :  : :  .          
-                       ..  ; :: .            
-                          ... .. :..         
-                         ::: :...            
-                     ::.:.:...;; .....       
-                  :..     .;.. :;     ..     
-                        . :. .  ;.           
-                         .: ;;: ;.           
-                        :; .BRRRV;           
-                           YB BMMMBR         
-                          ;BVIMMMMMt         
-                    .=YRBBBMMMMMMMB          
-                  =RMMMMMMMMMMMMMM;          
-                ;BMMR=VMMMMMMMMMMMV.         
-               tMMR::VMMMMMMMMMMMMMB:        
-              tMMt ;BMMMMMMMMMMMMMMMB.       
-             ;MMY ;MMMMMMMMMMMMMMMMMMV       
-             XMB .BMMMMMMMMMMMMMMMMMMM:      
-             BMI +MMMMMMMMMMMMMMMMMMMMi      
-            .MM= XMMMMMMMMMMMMMMMMMMMMY      
-             BMt YMMMMMMMMMMMMMMMMMMMMi      
-             VMB +MMMMMMMMMMMMMMMMMMMM:      
-             ;MM+ BMMMMMMMMMMMMMMMMMMR       
-              tMBVBMMMMMMMMMMMMMMMMMB.       
-               tMMMMMMMMMMMMMMMMMMMB:        
-                ;BMMMMMMMMMMMMMMMMY          
-                  +BMMMMMMMMMMMBY:           
-                    :+YRBBBRVt; 
-    """)
-    quit()
-
-
-pieces_0 = {
-    "I":  [[0, 0, 0, 0],
-           [1, 1, 1, 1],
-           [0, 0, 0, 0],
-           [0, 0, 0, 0]],
-    "J":  [[1, 0, 0],
-           [1, 1, 1],
-           [0, 0, 0]],
-    "L":  [[0, 0, 1],
-           [1, 1, 1],
-           [0, 0, 0]],
-    "O":  [[1, 1],
-           [1, 1]],
-    "S":  [[0, 1, 1],
-           [1, 1, 0],
-           [0, 0, 0]],
-    "T":  [[0, 1, 0],
-           [1, 1, 1],
-           [0, 0, 0]],
-    "Z":  [[1, 1, 0],
-           [0, 1, 1],
-           [0, 0, 0]]
-}
-pieces_90 = {
-    "I":  [[0, 0, 1, 0],
-           [0, 0, 1, 0],
-           [0, 0, 1, 0],
-           [0, 0, 1, 0]],
-    "J":  [[0, 1, 1],
-           [0, 1, 0],
-           [0, 1, 0]],
-    "L":  [[0, 1, 0],
-           [0, 1, 0],
-           [0, 1, 1]],
-    "O":  [[1, 1],
-           [1, 1]],
-    "S":  [[0, 1, 0],
-           [0, 1, 1],
-           [0, 0, 1]],
-    "T":  [[0, 1, 0],
-           [0, 1, 1],
-           [0, 1, 0]],
-    "Z":  [[0, 0, 1],
-           [0, 1, 1],
-           [0, 1, 0]]
-}
-pieces_180 = {
-    "I":  [[0, 0, 0, 0],
-           [0, 0, 0, 0],
-           [1, 1, 1, 1],
-           [0, 0, 0, 0]],
-    "J":  [[0, 0, 0],
-           [1, 1, 1],
-           [0, 0, 1]],
-    "L":  [[0, 0, 0],
-           [1, 1, 1],
-           [1, 0, 0]],
-    "O":  [[1, 1],
-           [1, 1]],
-    "S":  [[0, 0, 0],
-           [0, 1, 1],
-           [1, 1, 0]],
-    "T":  [[0, 0, 0],
-           [1, 1, 1],
-           [0, 1, 0]],
-    "Z":  [[0, 0, 0],
-           [1, 1, 0],
-           [0, 1, 1]]
-}
-pieces_270 = {
-    "I":  [[0, 1, 0, 0],
-           [0, 1, 0, 0],
-           [0, 1, 0, 0],
-           [0, 1, 0, 0]],
-    "J":  [[0, 1, 0],
-           [0, 1, 0],
-           [1, 1, 0]],
-    "L":  [[1, 1, 0],
-           [0, 1, 0],
-           [0, 1, 0]],
-    "O":  [[1, 1],
-           [1, 1]],
-    "S":  [[1, 0, 0],
-           [1, 1, 0],
-           [0, 1, 0]],
-    "T":  [[0, 1, 0],
-           [1, 1, 0],
-           [0, 1, 0]],
-    "Z":  [[0, 1, 0],
-           [1, 1, 0],
-           [1, 0, 0]]
-}
-
-def printboard(tboard = None):
-    if tboard is None: tboard = board
-    for row in tboard:
-        line = ""
-        for col in row:
-            line += f"\033[31m█\033[0m"*2 if col != 0 else '█'*2
-        print(line)
-
-def printboardOrg(tboard = None):
-    if tboard is None: tboard = board
-    print("Int: 0  1  2  3  4  5  6  7  8  9")
-    for i, row in enumerate(tboard):
-        i = f'{i} ' if i < 10 else f'{i}'
-        print(f'{i}: {row}')
-    print("Int: 0  1  2  3  4  5  6  7  8  9")
+                                 .       :       
+                                 :      .        
+                        :..   :  : :  .          
+                           ..  ; :: .            
+                              ... .. :..         
+                             ::: :...            
+                         ::.:.:...;; .....       
+                      :..     .;.. :;     ..     
+                            . :. .  ;.           
+                             .: ;;: ;.           
+                            :; .BRRRV;           
+                               YB BMMMBR         
+                              ;BVIMMMMMt         
+                        .=YRBBBMMMMMMMB          
+                      =RMMMMMMMMMMMMMM;          
+                    ;BMMR=VMMMMMMMMMMMV.         
+                   tMMR::VMMMMMMMMMMMMMB:        
+                  tMMt ;BMMMMMMMMMMMMMMMB.       
+                 ;MMY ;MMMMMMMMMMMMMMMMMMV       
+                 XMB .BMMMMMMMMMMMMMMMMMMM:      
+                 BMI +MMMMMMMMMMMMMMMMMMMMi      
+                .MM= XMMMMMMMMMMMMMMMMMMMMY      
+                 BMt YMMMMMMMMMMMMMMMMMMMMi      
+                 VMB +MMMMMMMMMMMMMMMMMMMM:      
+                 ;MM+ BMMMMMMMMMMMMMMMMMMR       
+                  tMBVBMMMMMMMMMMMMMMMMMB.       
+                   tMMMMMMMMMMMMMMMMMMMB:        
+                    ;BMMMMMMMMMMMMMMMMY          
+                      +BMMMMMMMMMMMBY:           
+                        :+YRBBBRVt; 
+        """)
+        quit()
 
 
 
 
-def randomPieceType():
-    return random.choice(list(pieces_0.keys()))
+    def printboard(self, tboard = None):
+        if tboard is None: tboard = self.board
+        for row in tboard:
+            line = ""
+            for col in row:
+                line += f"\033[31m█\033[0m"*2 if col != 0 else '█'*2
+            print(line)
 
-def randomNewPiece(ptype):
-    ptype = ptype if ptype is not None else randomPieceType()
-    x = 5 - round(len(pieces_0[ptype][0])/2)
-    return {
-        'pos': [0, x],
-        'type': ptype,
-        'rot': 0
-    }
-
-
-piece = randomNewPiece(None)
-piece_next = randomPieceType()
-piece_held = randomPieceType()
-canHold = True
-
-
-def getPieceArray():
-    if piece['rot'] == 0:
-        return pieces_0[piece['type']]
-    elif piece['rot'] == 90:
-        return pieces_90[piece['type']]
-    elif piece['rot'] == 180:
-        return pieces_180[piece['type']]
-    elif piece['rot'] == 270:
-        return pieces_270[piece['type']]
-    return "WTF WIE"
-
-
-def tempPutOnBoard():
-    tboard = copy.deepcopy(board)
-
-    piece_array = getPieceArray()
-
-    for rowint, row in enumerate(piece_array):
-        for colint, col in enumerate(row):
-            if col == 1: tboard[rowint + piece['pos'][0]][colint + piece['pos'][1]] = 2
-    return tboard
-
-def putOnBoard():
-    piece_array = getPieceArray()
-
-    for rowint, row in enumerate(piece_array):
-        for colint, col in enumerate(row):
-            if col == 1: board[rowint + piece['pos'][0]][colint + piece['pos'][1]] = 1
-
-
-def checkWallCollision():
-    for rowint, row in enumerate(getPieceArray()):
-        for colint, col in enumerate(row):
-            # FÜR JEDEN TEIL DES PIECES
-            if col == 1:
-                print()
-                if colint + piece['pos'][1] < 0: return True
-                if colint + piece['pos'][1] > 9: return True
-    return False
-
-def checkOtherPieceAndFloorCollision():
-    piece_array = getPieceArray()
-    for rowint, row in enumerate(piece_array):
-        for colint, col in enumerate(row):
-            # FÜR JEDEN TEIL DES PIECES
-            if col == 1:
-                if rowint + piece['pos'][0] > 19:
-                    return True
-                # WENN POS IM BOARD VON DEM TEIL BELEGT
-                if board[rowint + piece['pos'][0]][colint + piece['pos'][1]] != 0:
-                    return True
-
-    return False
-
-def nextPiece():
-    global piece, piece_next
-    piece = randomNewPiece(piece_next)
-    piece_next = randomPieceType()
-
-def clearlines():
-    for rowint ,row in enumerate(board):
-        if row == [1]*10:
-            board.pop(rowint)
-            board.insert(0, [0]*10)
+    def printboardOrg(self, tboard = None):
+        if tboard is None: tboard = self.board
+        print("Int: 0  1  2  3  4  5  6  7  8  9")
+        for i, row in enumerate(tboard):
+            i = f'{i} ' if i < 10 else f'{i}'
+            print(f'{i}: {row}')
+        print("Int: 0  1  2  3  4  5  6  7  8  9")
 
 
 
-while True:
-    printboard(tempPutOnBoard())
-    action = input(f"HELD: {piece_held} | NEXT: {piece_next} -> ").upper()
-    action_type = ""
 
-    if action == 'A':
-        piece['pos'][1] -= 1
-        action_type = "move"
-    if action == 'D':
-        piece['pos'][1] += 1
-        action_type = "move"
+    def randomPieceType(self):
+        return random.choice(list(self.pieces_0.keys()))
 
-
-    if action == "Q":
-        piece['rot'] -= 90
-        if piece['rot'] == -90: piece['rot'] = 270
-        action_type = "rotate"
-    if action == "E":
-        piece['rot'] += 90
-        if piece['rot'] == 360: piece['rot'] = 0
-        action_type = "rotate"
-
-    if action == "W":
-        if canHold:
-            piece_tmp = copy.deepcopy(piece)
-            piece = randomNewPiece(piece_held)
-            piece_held = piece_tmp['type']
-            action_type = "hold"
-            canHold = False
-        else:
-            print("Cannot hold")
-            boom()
+    def randomNewPiece(self, ptype):
+        ptype = ptype if ptype is not None else self.randomPieceType()
+        x = 5 - round(len(self.pieces_0[ptype][0])/2)
+        return {
+            'pos': [0, x],
+            'type': ptype,
+            'rot': 0
+        }
 
 
-    if action == 'S':
-        piece['pos'][0] += 1
-        action_type = "down"
-
-    if action == 'SS':
-        while not checkOtherPieceAndFloorCollision():
-            piece['pos'][0] += 1
-        action_type = "down"
 
 
-    if checkWallCollision():
-        print("Collision with wall")
-        boom()
 
-    if action_type == "move":
-        if checkOtherPieceAndFloorCollision():
-            print("Moved into other block")
-            boom()
-    if action_type == "rotate":
-        if checkOtherPieceAndFloorCollision():
-            print("Rotated into other block")
-            boom()
+    def getPieceArray(self):
+        if self.piece['rot'] == 0:
+            return self.pieces_0[self.piece['type']]
+        elif self.piece['rot'] == 90:
+            return self.pieces_90[self.piece['type']]
+        elif self.piece['rot'] == 180:
+            return self.pieces_180[self.piece['type']]
+        elif self.piece['rot'] == 270:
+            return self.pieces_270[self.piece['type']]
+        return "WTF WIE"
 
-    if action_type == "down":
-        if checkOtherPieceAndFloorCollision():
-            piece['pos'][0] -= 1
-            putOnBoard()
-            nextPiece()
-            canHold = True
-            clearlines()
+
+    def tempPutOnBoard(self):
+        tboard = copy.deepcopy(self.board)
+
+        piece_array = self.getPieceArray()
+
+        for rowint, row in enumerate(piece_array):
+            for colint, col in enumerate(row):
+                if col == 1: tboard[rowint + self.piece['pos'][0]][colint + self.piece['pos'][1]] = 2
+        return tboard
+
+    def putOnBoard(self):
+        piece_array = self.getPieceArray()
+
+        for rowint, row in enumerate(piece_array):
+            for colint, col in enumerate(row):
+                if col == 1: self.board[rowint + self.piece['pos'][0]][colint + self.piece['pos'][1]] = 1
+
+
+    def checkWallCollision(self):
+        for rowint, row in enumerate(self.getPieceArray()):
+            for colint, col in enumerate(row):
+                # FÜR JEDEN TEIL DES PIECES
+                if col == 1:
+                    print()
+                    if colint + self.piece['pos'][1] < 0: return True
+                    if colint + self.piece['pos'][1] > 9: return True
+        return False
+
+    def checkOtherPieceAndFloorCollision(self):
+        piece_array = self.getPieceArray()
+        for rowint, row in enumerate(piece_array):
+            for colint, col in enumerate(row):
+                # FÜR JEDEN TEIL DES PIECES
+                if col == 1:
+                    if rowint + self.piece['pos'][0] > 19:
+                        return True
+                    # WENN POS IM BOARD VON DEM TEIL BELEGT
+                    if self.board[rowint + self.piece['pos'][0]][colint + self.piece['pos'][1]] != 0:
+                        return True
+
+        return False
+
+    def nextPiece(self):
+        self.piece = self.randomNewPiece(self.piece_next)
+        self.piece_next = self.randomPieceType()
+
+    def clearlines(self):
+        for rowint ,row in enumerate(self.board):
+            if row == [1]*10:
+                self.board.pop(rowint)
+                self.board.insert(0, [0]*10)
+
+
+    def start(self):
+        while True:
+            self.printboard(self.tempPutOnBoard())
+            action = input(f"HELD: {self.piece_held} | NEXT: {self.piece_next} -> ").upper()
+            action_type = ""
+
+            if action == 'A':
+                self.piece['pos'][1] -= 1
+                action_type = "move"
+            if action == 'D':
+                self.piece['pos'][1] += 1
+                action_type = "move"
+
+
+            if action == "Q":
+                self.piece['rot'] -= 90
+                if self.piece['rot'] == -90: self.piece['rot'] = 270
+                action_type = "rotate"
+            if action == "E":
+                self.piece['rot'] += 90
+                if self.piece['rot'] == 360: self.piece['rot'] = 0
+                action_type = "rotate"
+
+            if action == "W":
+                if self.canHold:
+                    piece_tmp = copy.deepcopy(self.piece)
+                    self.piece = self.randomNewPiece(self.piece_held)
+                    self.piece_held = piece_tmp['type']
+                    action_type = "hold"
+                    self.canHold = False
+                else:
+                    print("Cannot hold")
+                    self.boom()
+
+
+            if action == 'S':
+                self.piece['pos'][0] += 1
+                action_type = "down"
+
+            if action == 'SS':
+                while not self.checkOtherPieceAndFloorCollision():
+                    self.piece['pos'][0] += 1
+                action_type = "down"
+
+
+            if self.checkWallCollision():
+                print("Collision with wall")
+                self.boom()
+
+            if action_type == "move":
+                if self.checkOtherPieceAndFloorCollision():
+                    print("Moved into other block")
+                    self.boom()
+            if action_type == "rotate":
+                if self.checkOtherPieceAndFloorCollision():
+                    print("Rotated into other block")
+                    self.boom()
+
+            if action_type == "down":
+                if self.checkOtherPieceAndFloorCollision():
+                    self.piece['pos'][0] -= 1
+                    self.putOnBoard()
+                    self.nextPiece()
+                    self.canHold = True
+                    self.clearlines()
+
+
+tetris = Tetris()
+tetris.start()
